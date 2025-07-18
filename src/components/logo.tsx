621a1522d65d53635d29674f02f5e79e68a94cbe
Goodluck Stanley <goodluckstanley20@gmail.com>
2025-07-18 15:20:00 +0000

    another one

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,8 @@
-import { BrainCircuit } from 'lucide-react';
+import { Drama } from 'lucide-react';
 import React from 'react';
 
 export function Logo({className}: {className?: string}) {
   return (
-    <BrainCircuit className={className || 'h-8 w-8'} />
+    <Drama className={className || 'h-8 w-8'} />
   );
 }
