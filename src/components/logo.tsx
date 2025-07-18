16fcfdb6fe791445d792df7fa3ee0d6b54780ae3
Goodluck Stanley <goodluckstanley20@gmail.com>
2025-07-18 15:20:48 +0000

    use the first one

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,8 @@
-import { Drama } from 'lucide-react';
+import { Brain } from 'lucide-react';
 import React from 'react';
 
 export function Logo({className}: {className?: string}) {
   return (
-    <Drama className={className || 'h-8 w-8'} />
+    <Brain className={className || 'h-8 w-8'} />
   );
 }
